737cc16dc49c06b2ec5e148f4e413a56140d0b36
Le Minh Tri <ansidev@gmail.com>
2023-01-07 08:52:51 +0700

    refactor: extract information to src/site.config.ts

---
+++ src/site.config.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'ansidev\'s page speedometer 🚀',
+  description: 'Benchmarking the performance of ansidev\'s sites over time.',
+  author: 'ansidev',
+  authorURL: 'https://ansidev.xyz',
+  sourceCodeProvider: 'GitHub',
+  repositoryURL: 'https://github.com/ansidev/pagespeed',
+  license: 'MIT',
+  licenseURL: 'https://github.com/ansidev/pagespeed/blob/main/LICENSE',
+  originalRepository: 'Speedlify for Astro sites',
+  originalAuthor: 'delucis',
+  originalAuthorURL: 'http://chrisswithinbank.net/',
+  originalRepositoryURL: 'https://github.com/delucis/hyperdrive-speedometer',
+}
